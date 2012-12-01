d6a8fee0d3ca71e925dcfc66a1c7b5ee4f64a798
Felix_Li <cs150-bs@p380-31.EECS.Berkeley.EDU>
2012-12-01 03:03:27 -0800

    pixel feeder seems off

--- software/GPcode/start.s
+++ software/GPcode/start.s
@@ -8,13 +8,13 @@ _start:
 	la $t1, 0x01ffffff
 	sw $t1, 0($t0)
 	nop
-	la $t1, 0x02000000
+	la $t1, 0x02ff0000
 	sw $t1, 4($t0)
 	nop
 	la $t1, 0x00000000
 	sw $t1, 8($t0)
 	nop
-	la $t1, 0x001A002B
+	la $t1, 0x03200258
 	sw $t1, 12($t0)
 	nop
 	la $t1, 0x02ffffff
@@ -23,7 +23,7 @@ _start:
 	la $t1, 0x01230124
 	sw $t1, 20($t0)
 	nop
-	la $t1, 0x00AA00BB
+	la $t1, 0x02580320
 	sw $t1, 24($t0)
 	nop
 	sw $0, 28($t0)
