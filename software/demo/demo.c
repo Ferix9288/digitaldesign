0f883d013835dac38df0ef2390e58f6a773fb243
Felix_Li <cs150-bs@p380-37.EECS.Berkeley.EDU>
2012-12-06 06:14:39 -0500

    smooth obj motion and ISR registering.

--- software/demo/demo.c
+++ software/demo/demo.c
@@ -1,76 +1,111 @@
-#define PIXEL_FRAME *((volatile unsigned int*) 0x8000020)
+#define PIXEL_FRAME *((volatile unsigned int*) 0x80000020)
 #define STATE ((volatile unsigned int*) 0x1beef000)
+#define INDEX ((volatile unsigned int*) 0x1beef004)
 
-#define CMD_ADDR_1 ((volatile unsigned int*) 0x17800000)
-#define CMD_ADDR_2 ((volatile unsigned int*) 0x19000000)
 
-#define GP_CODE ((volatile unsigned int*) 0x180000040)
+#define CMD_1 ((volatile unsigned int*) 0x17800000)
+#define CMD_2 ((volatile unsigned int*) 0x19000000)
+
+#define CMD_1_ADDR 0x17800000
+#define CMD_2_ADDR 0x19000000
+
+
+#define GP_CODE ((volatile unsigned int*) 0x18000000)
 #define GP_FRAME ((volatile unsigned int*) 0x18000004)
+
 
 struct player {
    int x;
    int y;
 };
 
+void drawBorder() {
+}
 
-int drawPlayer(struct player* playa, int x_dir, int y_dir) {
+void drawBlock(int x1, int y1, int x2, int y2, 
+	       int color, int height, unsigned int* cmd_addr) {
+  for (int i = y1; i <= y1 + height ; i++) {
+    cmd_addr[*INDEX] = 0x02 + color;
+    cmd_addr[*INDEX+1] = x1 << 16 + (y1 + i);
+    cmd_addr[*INDEX+2] = x2 << 16 + (y2 + i);
+    INDEX[0] = *INDEX + 3;
+  }
+  cmd_addr[*INDEX] = 0x00000000;
+  INDEX[0] = *INDEX + 1;
+}
+
+void drawPlayer(struct player* playa, int x_dir, 
+	       int y_dir,  unsigned int* cmd_addr) {
   playa->x = playa->x + x_dir;	
   playa->y = playa->y + y_dir;
   
   //0x1000 + 1 << 3 = 0x1001?
   //GP_instructions that draws player
-  CMD_ADDR_1[0] = 0x01000000;
-  CMD_ADDR_1[1] = 0x03ffffff;
-  CMD_ADDR_1[2] = 0x0000000a + (playa->y <<12) + (playa->x << 22);
-  CMD_ADDR_1[3] = 0x00000000;
-  return 0;
+  cmd_addr[*INDEX] = 0x01000000;
+  cmd_addr[*INDEX+1] = 0x03ff0000;
+  cmd_addr[*INDEX+2] = 0x00000030 + (playa->y <<12) + (playa->x << 22);
+  cmd_addr[*INDEX+3] = 0x00000000;
+  INDEX[0] = *INDEX+4;
 }
 
 
 int main(void) {
   unsigned int Current_Pixel_Frame, GP_Code_addr;
-  unsigned int currentFrame, oldFrame;
+  unsigned int currentFrame, oldFrame, demo_frame;
   unsigned int gameStateChange;
+  unsigned int* GP_Code_instr;
+
   struct player p1 = {400, 550};
+  drawPlayer(&p1, 0, 0, CMD_1);
+  
+  demo_frame = (PIXEL_FRAME == 0x10400000)? 0x10800000: 0x10400000;
+  GP_Code_instr = (demo_frame == 0x10400000)? CMD_1: CMD_2;
+  GP_Code_addr = (demo_frame == 0x10400000)? CMD_1_ADDR: CMD_2_ADDR;
+
+  GP_FRAME[0] = demo_frame;
+  GP_CODE[0] = GP_Code_addr;
+
+  INDEX[0] = 0;
   for ( ; ; ) {
+    INDEX[0] = 0;
     gameStateChange = 1;
+    demo_frame = (PIXEL_FRAME == 0x10400000)? 0x10800000: 0x10400000;
+    GP_Code_instr = (demo_frame == 0x10400000)? CMD_1: CMD_2;
+    GP_Code_addr = (demo_frame == 0x10400000)? CMD_1_ADDR: CMD_2_ADDR;
+    
+    drawBlock(0, 0, 800, 0, 0x00ffffff, 20, GP_Code_instr);
     switch (*STATE)
       {
       case 'w':
-	//add player up by one
-	drawPlayer(&p1, 0, -1);
-	break;
+    	//add player up by one
+    	drawPlayer(&p1, 0, -3, GP_Code_instr);
+    	break;
       case 'a':
-	//move player to the left
-	drawPlayer(&p1, -1, 0);
-	break;
+    	//move player to the left
+    	drawPlayer(&p1, -3, 0, GP_Code_instr);
+    	break;
       case 's':
-	//move player down by one
-	drawPlayer(&p1, 0, 1);
-	break;
+    	//move player down by one
+    	drawPlayer(&p1, 0, 3, GP_Code_instr);
+    	break;
       case 'd':
-	//move player to the right by one
-	drawPlayer(&p1, 1, 0);
-	break;
+    	//move player to the right by one
+    	drawPlayer(&p1, 3, 0, GP_Code_instr);
+    	break;
       default:
-	//do nothing
+    	//do nothing (i.e. don't move)
 	gameStateChange = 0;
-	break;
-	;}
-    if (gameStateChange) { 	
-      if (PIXEL_FRAME == 0x10400000) {
-	GP_FRAME[0] = 0x10800000;
-	GP_CODE[0] = GP_Code_addr;
-      } else {
-	GP_FRAME[0] = 0x10400000;
-	GP_CODE[0] = GP_Code_addr;
-      }
-    }
+	drawPlayer(&p1, 0, 0, GP_Code_instr);
+    	break;
+    	;}
+    STATE[0] = 'p';
     oldFrame = PIXEL_FRAME;
+    GP_FRAME[0] = demo_frame;
+    GP_CODE[0] = GP_Code_addr;
     while (PIXEL_FRAME == oldFrame);
-
   }
   return 0;
+
 }
 
 
